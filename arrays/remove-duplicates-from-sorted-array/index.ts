9791c3a6c42ba62e6d620a720eaf0475ef859979
pratikkshirsagar <meetpratiik@gmail.com>
2026-03-25 20:12:00 +0530

    added remove duplicates from sorted array

---
+++ arrays/remove-duplicates-from-sorted-array/index.ts
@@ -0,0 +1,12 @@
+function removeDuplicates(nums: number[]) {
+  let x = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > nums[x]) {
+      x = x + 1;
+      nums[x] = nums[i];
+    }
+  }
+
+  return x + 1;
+}
