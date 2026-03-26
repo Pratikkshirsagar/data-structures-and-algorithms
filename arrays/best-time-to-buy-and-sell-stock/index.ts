c277a4732716c677f13327b712892b07de13c3d7
pratikkshirsagar <meetpratiik@gmail.com>
2026-03-26 15:51:13 +0530

    added best time to buy and sell stock

---
+++ arrays/best-time-to-buy-and-sell-stock/index.ts
@@ -0,0 +1,21 @@
+function maxProfit(prices: number[]): number {
+  let min = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    const profit = prices[i] - min;
+
+    if (profit > maxProfit) {
+      maxProfit = profit;
+    }
+
+    if (prices[i] < min) {
+      min = prices[i];
+    }
+  }
+
+  return maxProfit;
+}
+
+const result = maxProfit([7, 1, 5, 3, 6, 4]);
+console.log(result);
