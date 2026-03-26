0d71333392b63b5430f6280d1782dde6451f6cc8
pratikkshirsagar <meetpratiik@gmail.com>
2026-03-26 15:25:23 +0530

    added reverse string

---
+++ arrays/reverse-string/index.ts
@@ -0,0 +1,12 @@
+function reverseString(s: string[]): void {
+  const length = s.length;
+  const halflength = Math.floor(length / 2);
+
+  for (let i = 0; i < halflength; i++) {
+    const temp = s[i];
+    s[i] = s[length - 1 - i];
+    s[length - 1 - i] = temp;
+  }
+}
+
+reverseString(["h", "e", "l", "l", "o"]);
