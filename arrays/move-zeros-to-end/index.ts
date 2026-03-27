257137d28bb8e32dce4cecdb080643911576a828
pratikkshirsagar <meetpratiik@gmail.com>
2026-03-27 15:03:03 +0530

    added move zero to end

---
+++ arrays/move-zeros-to-end/index.ts
@@ -0,0 +1,16 @@
+function moveZeroes(nums: number[]) {
+  let x = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[x] = nums[i];
+      x++;
+    }
+  }
+
+  for (let j = x; j < nums.length; j++) {
+    nums[j] = 0;
+  }
+}
+
+moveZeroes([0, 1, 4, 0, 5, 2]);
