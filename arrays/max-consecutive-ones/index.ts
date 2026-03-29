59dd0ed787266f31ebf74699a10823d705402658
pratikkshirsagar <meetpratiik@gmail.com>
2026-03-30 01:29:17 +0530

    added maxmConsecutiveo ones

---
+++ arrays/max-consecutive-ones/index.ts
@@ -0,0 +1,17 @@
+function findMaxConsecutiveOnes(nums: number[]): number {
+  let maxCount = 0;
+  let currentCount = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) {
+      currentCount++;
+    } else {
+      maxCount = Math.max(currentCount, maxCount);
+      currentCount = 0;
+    }
+  }
+
+  return Math.max(currentCount, maxCount);
+}
+
+console.log(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]));
