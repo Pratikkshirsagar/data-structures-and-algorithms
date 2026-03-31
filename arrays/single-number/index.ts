0d96ad150bf6fb725d87f6c83a7e164fc1593e3b
pratikkshirsagar <meetpratiik@gmail.com>
2026-03-31 19:49:43 +0530

    added single number

---
+++ arrays/single-number/index.ts
@@ -0,0 +1,12 @@
+// Question
+// https://leetcode.com/problems/single-number
+
+function singleNumber(nums: number[]): number {
+  let xor = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    xor = xor ^ nums[i];
+  }
+
+  return xor;
+}
